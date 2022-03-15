3ed69d739c75b01ec4343a048d19cb5d0047f16f
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-16 00:12:53 +0700

    Update ProjectDesc.js

--- components/Projects/ProjectDesc.js
+++ components/Projects/ProjectDesc.js
@@ -3,7 +3,7 @@ export default function ProjectsDesc(){
         <>
         <div className="md:w-4/12 w-10/12 md:mx-0 mx-auto md:text-left">
             <h1 className="text-2xl text-black font-bold mb-2">Mitrasoftware</h1>
-            <p className="text-base text-justify text-gray-500 font-poppins">Mitrasoftware adalah suatu aplikasi kasir sederhana berbasis web yang dibuat menggunakan Flask, Bootstrap, dan Javascript. Mitrasoftware menggunakan database PostgreSQL yang disediakan oleh Heroku. Anda dapat mengakses project saya dengan username dan password yang saya sediakan.</p>
+            <p className="text-base text-justify text-gray-500 font-poppins">Mitrasoftware is a simple web-based cashier application made using Flask, Bootstrap, and Javascript. Mitrasoftware uses the PostgreSQL database provided by Heroku. You can access my project with the username and password I provided.</p>
             <br/>
             <p className="text-base font-bold text-justify text-gray-500 font-poppins">Username : admin</p>
             <p className="text-base font-bold text-justify text-gray-500 font-poppins">Password : admin123</p>
