c055045d89650ea0d49fa268f1160978442fc036
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-15 23:13:04 +0700

    Create ProfileHeader.js

---
+++ components/Profile/ProfileHeader.js
@@ -0,0 +1,11 @@
+import SubTittle from "../GlobalComponents/SubTittle";
+import Tittle from "../GlobalComponents/Tittle";
+
+export default function ProfileHeader(){
+    return(
+        <>
+        <Tittle>Profile</Tittle>
+        <SubTittle>Tentang saya</SubTittle>
+        </>
+    )
+}
