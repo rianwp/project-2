33a0bfa58725fb4df7f6d192c13f102d7dfc45fc
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-15 23:13:28 +0700

    Create SkillsHeader.js

---
+++ components/Skills/SkillsHeader.js
@@ -0,0 +1,11 @@
+import SubTittle from "../GlobalComponents/SubTittle";
+import Tittle from "../GlobalComponents/Tittle";
+
+export default function SkillsHeader(){
+    return(
+        <>
+        <Tittle>Skills</Tittle>
+        <SubTittle>Beberapa kemampuan yang saya kuasai</SubTittle>
+        </>
+    )
+}
