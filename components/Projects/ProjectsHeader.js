261297ab69b7a107b366541b9f82375424dc88d4
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-15 23:13:14 +0700

    Create ProjectsHeader.js

---
+++ components/Projects/ProjectsHeader.js
@@ -0,0 +1,11 @@
+import SubTittle from "../GlobalComponents/SubTittle";
+import Tittle from "../GlobalComponents/Tittle";
+
+export default function ProjectsHeader(){
+    return(
+        <>
+        <Tittle>Project</Tittle>
+        <SubTittle>Project pertama saya</SubTittle>
+        </>
+    )
+}
