a6c732d5364371317e7ba313576b530fc157a44b
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-15 23:12:44 +0700

    Create MenuButton.js

---
+++ components/Hero/MenuButton.js
@@ -0,0 +1,13 @@
+import Button from "../GlobalComponents/Button"
+
+export default function MenuButton({variant, click}){
+    const size = "24px";
+    if(variant === "close"){
+        return <Button click={click}><svg className="fill-white opacity-60 hover:opacity-100" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width={size} height={size}><path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"/></svg></Button>
+    }
+    return(
+        <>
+        <Button click={click} className="sm:hidden"><svg className="fill-white opacity-60 hover:opacity-100" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width={size} height={size}><path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"/></svg></Button>
+        </>
+    )
+}
