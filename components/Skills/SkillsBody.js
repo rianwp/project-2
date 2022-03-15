625ada996bb1753e4972f0883d16e5960a56b68f
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-16 00:13:03 +0700

    Update SkillsBody.js

--- components/Skills/SkillsBody.js
+++ components/Skills/SkillsBody.js
@@ -4,10 +4,10 @@ export default function SkillsBody(){
     return(
         <>
         <section className="flex flex-wrap py-8 justify-center">
-            <SkillBox logo="./python.svg" level="Menengah">Python</SkillBox>
-            <SkillBox logo="./js.svg" level="Pemula">Javascript</SkillBox>
-            <SkillBox logo="./react.svg" level="Pemula">React</SkillBox>
-            <SkillBox logo="./flask.svg" level="Menengah">Flask</SkillBox>
+            <SkillBox logo="./python.svg" level="Average">Python</SkillBox>
+            <SkillBox logo="./js.svg" level="Beginner">Javascript</SkillBox>
+            <SkillBox logo="./react.svg" level="Beginner">React</SkillBox>
+            <SkillBox logo="./flask.svg" level="Average">Flask</SkillBox>
         </section>
         </>
     )
