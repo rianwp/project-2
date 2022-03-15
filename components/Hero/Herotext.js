4042492de8193a5c4d066dad6f3fcf3102919ab0
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-16 00:12:46 +0700

    Update Herotext.js

--- components/Hero/Herotext.js
+++ components/Hero/Herotext.js
@@ -9,8 +9,8 @@ export default function Herotext(){
     return(
         <>
         <div className={`mx-auto sm:w-8/12 w-10/12 sm:mt-40 mt-24 text-center transition duration-1000 ${setOpening}`}>
-            <h1 className="text-white text-2xl inline-block font-poppins font-semibold tracking-widest">Saya adalah seorang web developer pemula yang sedang belajar dan berkembang hingga saat ini.</h1>
-            <p className="mt-6 opacity-60 text-white text-base inline-block font-poppins">Mulai tertarik dan belajar web development saat berumur 18 tahun</p>
+            <h1 className="text-white text-2xl inline-block font-poppins font-semibold tracking-widest">I am a novice web developer who is currently learning and developing.</h1>
+            <p className="mt-6 opacity-60 text-white text-base inline-block font-poppins">Interested and learn web development at the age of 18 years</p>
         </div>
         </>
     )
