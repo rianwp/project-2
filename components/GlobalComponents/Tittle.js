e8644a98f9f16cbe7715c694b8e6f3367589f398
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-15 23:12:34 +0700

    Create Tittle.js

---
+++ components/GlobalComponents/Tittle.js
@@ -0,0 +1,7 @@
+export default function Tittle({children}){
+    return(
+        <>
+        <h1  className="font-poppins text-xl text-black font-semibold tracking-widest">{children}</h1>
+        </>
+    )
+}
