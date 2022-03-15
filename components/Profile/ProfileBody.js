85c5a03d7d05752872fd97e8f09decc9d9f8bf24
rianwp <90314741+rianwp@users.noreply.github.com>
2022-03-15 23:13:00 +0700

    Create ProfileBody.js

---
+++ components/Profile/ProfileBody.js
@@ -0,0 +1,9 @@
+export default function ProfileBody(){
+    return(
+        <>
+        <div className="w-10/12 mx-auto py-8">
+            <p className="text-base text-justify text-gray-500 font-poppins">Perkenalkan, nama saya adalah Adrian Wahyu Prasetya. Saya adalah seorang mahasiswa jurusan Sistem Informasi di Universitas AMIKOM Yogyakarta. Saat umur 18 tahun, saya memiliki ketertarikan dengan dunia progamming. Diawali dengan ketertarikan saya dengan bahasa Python, saya mempelajari cukup banyak hal mencakup progamming dan sekarang saya sedang menekuni bidang web developing. Ketertarikan saya terhadap web developing membuat saya mempelajari banyak hal hingga saat ini, baik di sisi front-end atau back-end. Dalam masa perkembangan saya, saya mempelajari Flask dan React JS sebagai awal bagi saya dalam masuk ke bidang ini.</p>
+        </div>
+        </>
+    )
+}
